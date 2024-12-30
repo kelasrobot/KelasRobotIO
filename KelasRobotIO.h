0485cf5183e634a3978e6927fcf44f9b60030417
Ajang Rahmat <41977490+ajangrahmat@users.noreply.github.com>
2024-12-30 21:43:21 +0700

    add files cpp

---
+++ KelasRobotIO.h
@@ -0,0 +1,21 @@
+#ifndef KELASROBOTIO_h
+#define KELASROBOTIO_h
+
+#include "Arduino.h"
+
+void setupPin(byte numberPin, boolean statusMode){
+    pinMode(numberPin, statusMode);
+}
+
+void outputSet(byte numberPin, boolean statusPin){
+    digitalWrite(numberPin, statusPin);
+}
+
+void blink(byte numberPin, int delayBlink){
+    digitalWrite(numberPin, HIGH);
+    delay(delayBlink);
+    digitalWrite(numberPin, LOW);
+    delay(delayBlink);
+}
+
+#endif
